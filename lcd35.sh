8b66a35089a77d7e01b6da3bf703251203ba0f49
mereith <cs3cx4g@gamil.com>
2020-04-25 20:23:16 +0800

    ok.

---
+++ lcd35.sh
@@ -0,0 +1,85 @@
+#!/bin/bash
+
+echo "开启显示。"
+sudo cp /boot/config.txt ./config.txt.bak
+sudo cp /boot/cmdline.txt ./cmdline.txt.bak
+
+sudo echo "dtparam=i2c_arm=on" >> /boot/config.txt
+sudo echo "dtparam=spi=on" >> /boot/config.txt
+sudo echo "enable_uart=1" >> /boot/config.txt
+sudo echo "dtoverlay=tft35a:rotate=90" >> /boot/config.txt
+sudo mkdir /etc/X11/xorg.conf.d
+
+sudo cp ./tft35a-overlay.dtb /boot/overlays/tft35a.dtb
+
+sudo cp -rf ./99-calibration.conf-35-90  /etc/X11/xorg.conf.d/99-calibration.conf
+sudo cp -rf ./99-fbturbo.conf /usr/share/X11/xorg.conf.d/99-fbturbo.conf
+
+# add cmdline
+#sudo echo " fbcon=map:10 fbcon=font:ProFont6x11" >> /boot/cmdline.txt
+
+# touch
+#sudo apt install xinput
+#sudo apt install xinput-calibrator
+echo "更新一下"
+sudo apt update
+sudo apt upgrade
+echo "安装python3.8需要的库"
+sid-used sudo apt install libbz2-dev libc6-dev libffi-dev libgdbm-dev libgdbm-compat-dev liblzma-dev libncurses5-dev
+sid-used sudo apt install libreadline-dev libsqlite3-dev libssl-dev openssl sqlite3 tcl-dev tk-dev uuid-dev zlib1g-dev
+
+
+echo "下载python源码"
+sudo curl -O "https://www.python.org/ftp/python/3.8.2/Python-3.8.2.tgz"
+tar zxf ./Python-3.8.2.tgz
+cd ./Python-3.8.2.tgz
+./configure --enable-optimizations
+make -j 4
+sudo make install
+ptyhon3.8 --version
+pip3 --version
+sudo rm /usr/bin/python3
+sudo ln -s /usr/local/bin/python3.8 /usr/bin/python3
+
+echo "安装完成"
+
+echo "pyqt5"
+cd ~
+sudo apt install qt5-default qt5-qmake dbus-python
+curl -O https://files.pythonhosted.org/packages/4d/81/b9a66a28fb9a7bbeb60e266f06ebc4703e7e42b99e3609bf1b58ddd232b9/PyQt5-5.14.2.tar.gz
+tar vxzf PyQt5-5.14.2.tar.gz
+cd ./PyQt5-5.14.2
+sudo sip-install
+
+
+echo "显示屏显示一下子。"
+cd ~
+git clone https://github.com/mereithhh/my-raspberry
+cd ./my-raspberry
+sudo python3 ./show-info/main.py
+
+
+
+echo "配置samba"
+sudo apt install -y samba samba-common ntfs-3g
+
+pause
+#rebot 
+sudo sync
+sudo reboot
+
+
+
+
+
+
+
+
+
+
+
+ (tcl8.6-dev)
+ (tk8.6-dev)
+
+
+
---
+++ show-info/main.py
@@ -0,0 +1,151 @@
+
+# -*- coding: utf-8 -*-
+# @Date    : 2017-09-03 17:47:15
+# @Author  : The carver (aologe@qq.com)
+# @Link    : 
+# @Version : 0.0.0
+
+import os, sys, time, json
+from PyQt5.QtWidgets import (QApplication, QWidget,
+	QLabel, QFrame, QPushButton, QStatusBar, 
+	QHBoxLayout, QVBoxLayout, QLineEdit)
+from PyQt5.QtGui import QFont, QPalette, QPixmap, QBrush, QIcon, QFontDatabase
+from PyQt5.QtCore import QPoint, QSize, Qt, QCoreApplication, QBasicTimer
+
+VERSION = "0.0.0"
+UNIT = 1000
+MINSUNIT = 900
+WEEK = ["星期一", "星期二", "星期三", "星期四", "星期五", "星期六", "星期日"]
+SCHEDULE = {
+	"星期一":[],
+	"星期二":[],
+	"星期三":[],
+	"星期四":[],
+	"星期五":[],
+	"星期六":['兄弟，大联盟'],
+	"星期日":[],
+}
+
+class MyButton(QPushButton):
+	def __init__(self, father=None):  #path is a list
+		super().__init__(father)
+
+		
+		self.setFocusPolicy(Qt.NoFocus)
+		#self.setStyleSheet("QPushButton {background-color: transparent}")
+		
+		#self.setFlat(True)
+
+
+class Example(QWidget):
+
+	def __init__(self):
+		super().__init__()
+
+		self.initUI()
+
+	def initUI(self):
+
+		self.flush = 1 #初始化基本图像刷新值，由于首次启动程序背景图像从0开始，所以确保更新为下一张，从而直接从1开始
+
+
+		self.setWindowFlags(Qt.FramelessWindowHint)  # 设置窗口标记，使其解除标题栏
+
+		self.palette = QPalette()
+		self.palette.setBrush(QPalette.Background, QBrush(QPixmap("./icon/background/0.jpg")))
+		self.setAutoFillBackground(True)  # 设置自动填充满背景图
+		self.setPalette(self.palette)
+
+
+		#imagehox = QHBoxLayout()
+		vox = QVBoxLayout()
+
+		#self.btnclose = QPushButton()
+		#self.btnclose.setFont(QFont("Arial",50))
+		#self.btnclose.setGeometry(10, 100, 100, 100)
+
+		self.lbltime = QLabel(self)
+		self.lbltime.setFont(QFont("Arial",50))
+		self.lbltime.setGeometry(35, 175, 250, 61)
+		self.lbltime.setStyleSheet("QLabel {color: white}")
+
+		
+		self.lbldate = QLabel(self)
+		self.lbldate.setFont(QFont("Arial", 25))
+		self.lbldate.setGeometry(41, 230, 300, 41)
+		self.lbldate.setStyleSheet("QLabel {color: white}")
+		
+
+
+		self.lblwordr = QLabel(self)
+		self.lblwordr.setFont(QFont("Arial", 16))
+		self.lblwordr.setStyleSheet("QLabel {color: white; text-align: center;}")
+
+
+
+
+
+	
+
+		self.timer = QBasicTimer()
+		self.timer.start(UNIT, self)
+
+		
+		
+		vox.addStretch(1)
+		vox.addWidget(self.lblwordr)
+		
+	
+		
+
+		#self.btnclose.clicked.connect(self.close)
+
+		self.setLayout(vox)
+		self.setMinimumSize(480, 320)
+		self.setMaximumSize(480, 320)
+		self.move(0, 0)
+		self.show()
+		#self.showFullScreen()
+
+	def timerEvent(self, *args, **kwargs):
+		self.flush += 1
+
+		if self.flush % MINSUNIT == 0:
+			n = int(self.flush / MINSUNIT)
+			self.palette.setBrush(QPalette.Background, QBrush(QPixmap("./icon/background/" + str(n) + ".jpg")))
+			self.setPalette(self.palette)
+
+			if n == len(os.listdir("./icon/background"))-1:
+				self.flush = 0
+
+
+
+			
+
+		localtime = list(time.localtime(time.time()))
+		#year = localtime[0]
+		mon = localtime[1]
+		day = localtime[2]
+		hour = localtime[3]
+		mins = localtime[4]
+		sec = localtime[5]
+		wday = localtime[6]
+
+		todo_list = SCHEDULE[WEEK[wday]]
+
+		todo = str()
+		for i in todo_list:
+			todo += (i + ' ')
+
+
+		self.lbltime.setText("%02d:%02d:%02d" % (hour, mins,sec))
+		self.lbldate.setText("%d月%d日, %s" % (mon, day, WEEK[wday]))
+
+		self.lblwordr.setText(todo)
+
+if __name__ == '__main__':
+
+	app = QApplication(sys.argv)
+	#app.setOverrideCursor(Qt.BlankCursor)  #when you put your cursor over the app,hide your cursor
+	ex = Example()
+	sys.exit(app.exec_())
